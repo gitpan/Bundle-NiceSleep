307c0d95d3e919a32fe127469eb8836b1afa8209
Josh Rabinowitz <joshr-cpan@joshr.com>
2002-02-22 14:24:23 +0000

    Import of JOSHR/Bundle-NiceSleep-0.10 from CPAN.
    gitpan-cpan-distribution: Bundle-NiceSleep
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         JOSHR/Bundle-NiceSleep-0.10.tar.gz
    gitpan-cpan-author:       JOSHR
    gitpan-cpan-maturity:     released

---
+++ NiceSleep.pm
@@ -0,0 +1,49 @@
+package Bundle::NiceSleep;
+
+our $VERSION = '0.10';	# matches version of Proc::NiceSleep 
+
+# Preloaded methods go here.
+
+1;
+__END__
+
+=head1 NAME
+
+Bundle::NiceSleep - Perl Bundle for NiceSleep-related functionality.
+
+=head1 SYNOPSIS
+
+ C<perl -MCPAN -e 'install Bundle::NiceSleep'> 
+
+=head1 CONTENTS
+
+Proc::NiceSleep		
+
+Proc::ProcessTable
+
+Proc::Swarm
+
+Proc::Queue
+
+Time::HiRes
+
+
+=head1 DESCRIPTION
+
+Modules that are useful for controlling a spate of server processes.
+
+=head1 AUTHOR
+
+Josh Rabinowitz, E<lt>joshr-proc-nicesleep@joshr.comE<gt>
+
+=head1 SEE ALSO
+
+L<Proc::NiceSleep>, L<Time::HiRes>, L<Proc::ProcessInfo>, L<Proc::Swarm>, 
+L<Proc::Queue>.
+
+=head1 LICENSE
+
+Copyright (c) 2002 Josh Rabinowitz, All Right Reserved. Licensed the same as 
+Perl itself.
+
+=cut
